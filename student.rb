6f2db80144585fe8064d2b8b6693ad9b2f4e6112
gak92 <44802922+gak92@users.noreply.github.com>
2022-09-28 16:27:30 +0500

    Add has-many and belongs to relationship between Classroom and Student

--- student.rb
+++ student.rb
@@ -1,4 +1,5 @@
 require_relative 'person'
+require_relative 'classroom'
 
 class Student < Person
   attr_reader :classroom
@@ -6,6 +7,7 @@ class Student < Person
   def initialize(id, age, classroom, name = 'Unknown', parent_permission: true)
     super(id, age, name, parent_permission)
     @classroom = classroom
+    classroom.students << self
   end
 
   def play_hockey
