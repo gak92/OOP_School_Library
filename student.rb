a5967447a156f96714042d64121d01ebd1f3d52a
gak92 <44802922+gak92@users.noreply.github.com>
2022-09-28 20:12:01 +0500

    Update Student and Classroom functionality

--- student.rb
+++ student.rb
@@ -7,10 +7,14 @@ class Student < Person
   def initialize(id, age, classroom, name = 'Unknown', parent_permission: true)
     super(id, age, name, parent_permission)
     @classroom = classroom
-    classroom.students << self
   end
 
   def play_hockey
     '¯\(ツ)/¯'
+  end
+
+  def classroom(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
