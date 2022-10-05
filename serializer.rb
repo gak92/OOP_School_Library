3548d939ced93af533a5dfc9d273125c6c0d8664
usbbush <jeanbienvenusb@gmail.com>
2022-10-05 12:26:54 +0200

    Handle person of type teacher

--- serializer.rb
+++ serializer.rb
@@ -4,13 +4,23 @@ require 'json'
 class Serializer
   def self.to_string(arr)
     JSON.generate(arr.map do |person|
-                    { id: person.id, age: person.age, name: person.name, parent_permission: person.parent_permission }
-                  end)
+      if person.instance_of?(Student)
+        { id: person.id, age: person.age, name: person.name,
+          parent_permission: person.parent_permission }
+      else
+        { id: person.id, age: person.age, name: person.name, specialization: person.specialization }
+      end
+    end)
   end
 
   def self.to_object(arr)
-    arr.map do |person| 
-      new_person = Student.new(person['age'], person['name'], parent_permission: person['parent_permission'])
+    arr.map do |person|
+      new_person = if person['specialization'].nil?
+                     Student.new(person['age'], person['name'],
+                                 parent_permission: person['parent_permission'])
+                   else
+                     Teacher.new(person['age'], person['specialization'], person['name'])
+                   end
       new_person.id = person['id']
       new_person
     end
