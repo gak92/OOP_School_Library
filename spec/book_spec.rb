da216f0bd9fdab946b433383de17582ff1a973b2
gak92 <44802922+gak92@users.noreply.github.com>
2022-10-05 18:35:08 +0500

    Add Testing functionality to add the Book object

---
+++ spec/book_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Book do
+  before :each do
+    @book = Book.new("Game of Thrones", "George Martin")
+  end
+
+  describe "#new" do
+    it "takes two parameters and returns a Book object" do
+        @book.should be_an_instance_of Book
+    end
+  end
+
+  describe "#title" do
+    it "returns the correct title" do
+      @book.title.should eql "Game of Thrones"
+    end
+  end
+  
+  describe "#author" do
+    it "returns the correct author" do
+      @book.author.should eql "George Martin"
+    end
+  end
+
+end
