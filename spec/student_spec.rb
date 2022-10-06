a8c6e4be1c3d2465c1496636e302f513fd87f3a1
usbbush <jeanbienvenusb@gmail.com>
2022-10-06 09:00:52 +0200

    Add test for student instance

---
+++ spec/student_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe Student do
+  before :each do
+    @student = Student.new(20, 'ushindi')
+  end
+
+  describe '#new' do
+    it 'takes two parameters and one optional and returns a Student object' do
+      @student.should be_an_instance_of Student
+    end
+  end
+
+  describe '#age' do
+    it 'returns the correct age' do
+      @student.age.should eql  20
+    end
+  end
+
+  describe '#name' do
+    it 'returns the correct name' do
+      @student.name.should eql 'ushindi'
+    end
+  end
+  describe '#parent_permission' do
+    it 'returns the default parent_permission' do
+      @student.parent_permission.should eql true
+    end
+  end
+end
